404a11786c7a2efabb5c69ca8c3520f1dc6f84ac
Dmitry Kiselyov <dmitry.kiselyov.mail@gmail.com>
2023-02-18 01:47:25 +0100

    Center the main content

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,10 +2,11 @@ import { FC } from 'react';
 
 import { Cards } from "components/Cards";
 import { Header } from "components/Header";
+import styles from "./App.module.scss";
 
 export const App: FC = () => {
   return (
-    <div>
+    <div className={styles.app}>
       <Header />
       <Cards />
     </div>
